cd4f76eb97773cdf444f8f09f7d1361b69df850e
Julian Aiup <julianaiup@yahoo.com>
2016-04-15 15:28:11 -0300

    Added connection to db in app.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/user');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/crudtest');
+
 var app = express();
 
 var env = process.env.NODE_ENV || 'development';
